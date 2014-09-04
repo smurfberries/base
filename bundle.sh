c4d655088186c131c01912fc12ed010c02538ac7
smurfberri.es <admin@smurfberri.es>
2014-09-04 20:47:57 +1000

    making bundle say something...

--- bundle.sh
+++ bundle.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-if [ -z "$SBPATH" ]; then
+if [ "$SBPATH" = "" ]; then
 	echo '$SBPATH undefined. Set it';
 	exit 1;
 fi
 
 git archive master | tar -x -C $SBPATH -f -
+if [ $? -eq 0 ]; then
+	echo Done;
+else
+	echo Fail;
+fi
